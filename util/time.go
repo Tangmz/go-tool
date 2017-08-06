d073d5c3a3cf29b36e1638337febd4a1f9de89ae
tangs-drm <tangscna@gmail.com>
2017-08-06 17:51:40 +0800

    添加10位的时间戳

--- util/time.go
+++ util/time.go
@@ -2,7 +2,18 @@ package util
 
 import "time"
 
+// Now返回一个13位的时间戳,即单位是毫秒
 func Now() int64 {
 	now := time.Now().UnixNano()/10e6
 	return now
+}
+
+// Now10 返回一个10位的时间戳,即单位是秒
+func Now10() int64 {
+	return Now() / 1000
+}
+
+// Now13返回一个13位的时间戳,即单位是毫秒,同Now()
+func Now13() int64 {
+	return Now()
 }
