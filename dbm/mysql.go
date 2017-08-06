9fb38ed64e68bf63b1bb353886ba003bdbb2a9b1
tangs-drm <tangscna@gmail.com>
2017-08-06 14:12:43 +0800

    添加mysql的测试

--- dbm/mysql.go
+++ dbm/mysql.go
@@ -11,6 +11,7 @@ type DB struct  {
 
 var Db *DB
 
+// NewDB根据name新建一个数据库的对象
 func NewDB(name string, url string) (*DB, error) {
 	if len(name) < 1 {
 		return nil, util.Error("name is invalid: %v", name)
@@ -20,10 +21,11 @@ func NewDB(name string, url string) (*DB, error) {
 		return nil, err
 	}
 	var db_ = &DB{DB: db}
-	DbMangerV.Register(name, db_)
+	DefaultDBManger.Register(name, db_)
 	return db_, nil
 }
 
+// Default使用默认的数据库对象
 func Default(url string) (*DB, error) {
 	var err error
 	Db, err =  NewDB("default", url)
@@ -33,26 +35,43 @@ func Default(url string) (*DB, error) {
 	return Db, nil
 }
 
-var DbMangerV = NewDbManager()
+var DefaultDBManger = NewDbManager()
 
 type DbManager struct {
 	DbMap map[string]*DB
 }
 
+// NewDbManager 新建一个数据库对象管理者
 func NewDbManager() *DbManager {
 	return &DbManager{
 		DbMap:map[string]*DB{},
 	}
 }
 
+// Register 通过name注册数据库对象
 func (dm *DbManager) Register(name string, db *DB) {
 	dm.DbMap[name] = db
 }
 
+// Db 根据name返回对应的数据库对象,name为空则返回默认的数据库对象
 func (dm *DbManager) Db(name... string) *DB {
 	if len(name) < 1 {
 		return dm.DbMap["default"]
 	}
 	nm := name[0]
 	return dm.DbMap[nm]
+}
+
+// NewDBWithManager 新建数据库对象,并注册到指定的数据库管理者里
+func NewDBWithManager(manager *DbManager, name string, url string) (*DB, error) {
+	if len(name) < 1 {
+		return nil, util.Error("name is invalid: %v", name)
+	}
+	db, err := sql.Open("mysql", url)
+	if err != nil {
+		return nil, err
+	}
+	var db_ = &DB{DB: db}
+	manager.Register(name, db_)
+	return db_, nil
 }
--- dbm/mysql_test.go
+++ dbm/mysql_test.go
@@ -3,12 +3,75 @@ package dbm
 import (
 	"testing"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/tangs-drm/go-tool/log"
+	"github.com/tangs-drm/go-tool/util"
+	"fmt"
 )
 
 func TestMysql(t *testing.T) {
 	var err error
-	_, err = Default("user:123@tcp(127.0.0.1:3306)/test?charset=utf8")
+	var db *DB
+	db, err = Default("testUser:123@tcp(localhost:3306)/testdb?charset=utf8")
 	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = db.Ping()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	res, err := db.Exec("drop table if exists test;")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(res)
+	log.Debug("res1 -- %v", util.S2Json(res))
+
+	res, err = db.Exec("create table test(id int, val varchar(16));")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(res)
+	log.Debug("res2 -- %v", util.S2Json(res))
+
+	// 手动注册数据库
+	db, err = NewDB("", "")
+	if err == nil {
+		t.Error(err)
+		return
+	}
+
+	db = DefaultDBManger.Db()
+	if db == nil {
+		t.Error(err)
+		return
+	}
+
+	db = DefaultDBManger.Db("default")
+	if db == nil {
+		t.Error(nil)
+		return
+	}
+
+	db, err = NewDBWithManager(DefaultDBManger, "", "testUser:123@tcp(localhost:3306)/testdb?charset=utf8")
+	if err == nil {
+		t.Error(err)
+		return
+	}
+
+	db, err = NewDBWithManager(DefaultDBManger, "halo", "testUser:123@tcp(localhost:3306)/testdb?charset=utf8")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+
 		t.Error(err)
 		return
 	}
