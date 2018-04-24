47260d883d49a54faaf22247f682994b57ec422d
tangs <tangscna@gmail.com>
2018-04-24 23:48:08 +0800

    开始初步实现http

---
+++ network/http/http.go
@@ -0,0 +1,56 @@
+package http
+
+import "net/http"
+
+type ServeMux struct {
+	m    map[string]muxEntry
+	mAry []muxEntry
+}
+
+type muxEntry struct {
+	h        http.Handler
+	pattern  string
+	explicit bool
+}
+
+// DefaultServeMux is the default ServeMux used by Serve.
+var DefaultServeMux = &defaultServeMux
+
+var defaultServeMux ServeMux
+
+func Handle(pattern string, handler http.Handler) {
+	DefaultServeMux.Handle(pattern, handler)
+}
+
+func (mux *ServeMux) Handle(pattern string, handler http.Handler) {
+	if pattern == "" {
+		panic("http: invalid pattern " + pattern)
+	}
+	if handler == nil {
+		panic("http: nil handler")
+	}
+	if mux.m[pattern].explicit {
+		panic("http: multiple registrations for " + pattern)
+	}
+
+	if mux.m == nil {
+		mux.m = make(map[string]muxEntry)
+	}
+	me := muxEntry{h: handler, pattern: pattern}
+	if _, ok := mux.m[pattern]; ok {
+		for index, p := range mux.mAry {
+			if pattern == p.pattern {
+				mux.mAry[index] = me
+			}
+		}
+	}
+	mux.m[pattern] = me
+}
+
+func HandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	DefaultServeMux.HandleFunc(pattern, handler)
+}
+
+func (mux *ServeMux) HandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	mux.Handle(pattern, http.HandlerFunc(handler))
+}
