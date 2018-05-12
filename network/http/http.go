bf7b7358311fd077b8f611b95f4ed4643b8b66dc
tsdrm <tangscna@gmail.com>
2018-05-12 18:00:23 +0800

    修改http的的实现

--- network/http/http.go
+++ network/http/http.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"net/http"
+	"path"
 	"sync"
 )
 
@@ -61,15 +62,85 @@ func (mux *ServeMux) Handle(pattern string, handler http.Handler) {
 func (mux *ServeMux) addHandler(pattern string, handler http.Handler) {
 	var ent = entity{explicit: true, pattern: pattern, handler: handler}
 	mux.handler[pattern] = ent
-	for _, entry := range mux.handlers {
-		if pattern == entry.pattern {
-		}
-	}
 	mux.handlers = append(mux.handlers, ent)
+}
+
+func MiddleWareFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	DefaultServeMux.MiddleWare(pattern, http.Handler(handler))
+}
+
+func MiddleWare(pattern string, handler http.Handler) {
+	DefaultServeMux.MiddleWare(pattern, handler)
+}
+
+func (mux *ServeMux) MiddleWare(pattern string, handler http.Handler) {
+	mux.lock.Lock()
+	defer mux.lock.Unlock()
+
+	if pattern == "" {
+		panic("middleware: invalid pattern " + pattern)
+	}
+	if handler == nil {
+		panic("middleware: nil handler")
+	}
+	if mux.handler[pattern].explicit {
+		panic("middleware: multiple registrations for " + pattern)
+	}
+
+	if mux.handler == nil {
+		mux.handler = make(map[string]entity)
+	}
+
+	mux.addMiddleWare(pattern, handler)
+}
+
+func (mux *ServeMux) addMiddleWare(pattern string, handler http.Handler) {
+	var ent = entity{explicit: true, pattern: pattern, handler: handler}
+	mux.middleWare[pattern] = ent
+	mux.middleWares = append(mux.middleWares, ent)
 }
 
 // ServeHTTP dispatches the request to the handler whose
 // pattern most closely matches the request URL.
-func (s *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (mux *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.RequestURI == "*" {
+		if r.ProtoAtLeast(1, 1) {
+			w.Header().Set("Connection", "close")
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	// find middleWare and exec it.
+	for _, m := range mux.middleWares {
+		path := cleanPath(r.URL.Path)
+		if m.pattern == path {
+			m.handler.ServeHTTP(w, r)
+		}
+	}
+
+	// find http handler and exec it.
+	for _, m := range mux.handlers {
+		path := cleanPath(r.URL.Path)
+		if m.pattern == path {
+			m.handler.ServeHTTP(w, r)
+		}
+	}
+}
+
+// Return the canonical path for p, eliminating . and .. elements.
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+	// path.Clean removes trailing slash except for root;
+	// put the trailing slash back if necessary.
+	if p[len(p)-1] == '/' && np != "/" {
+		np += "/"
+	}
+	return np
 }
