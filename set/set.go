06128084aca88e7b5a5bb21f3aacbbb87d323132
tsdrm <tangscna@gmail.com>
2018-05-12 17:14:23 +0800

    实现字符串数组的集合

--- set/set.go
+++ set/set.go
@@ -1,9 +1,20 @@
 package set
 
+//	The Set was implemented by golang.
 type Set interface {
+	// Determine if two values are equal. if it's true, val1 is equal to val2.
 	Equal(val1, val2 interface{}) bool
+
+	// Return the []interface{} that each element is the element of the origin Set.
 	Elements() []interface{}
+
+	// Add an unique element to Set.
 	Add(val interface{}) interface{}
+
+	// Remove the element from Set.
+	Remove(val interface{}) interface{}
+
+	// Return the length of the Set.
 	Len() int
 }
 
@@ -16,14 +27,8 @@ func Add(set Set, val interface{}) interface{} {
 	return set.Add(val)
 }
 
-// todo. This Method is not complete.
 func Remove(set Set, val interface{}) interface{} {
-	for _, s := range set.Elements() {
-		if set.Equal(s, val) {
-			break
-		}
-	}
-	return nil
+	return set.Remove(val)
 }
 
 func Len(set Set) int {
@@ -32,25 +37,37 @@ func Len(set Set) int {
 
 type StringSlice []string
 
-func (s StringSlice) Equal(val1, val2 interface{}) bool { return val1.(string) == val2.(string) }
-func (s StringSlice) Elements() []interface{} {
-	var strs = []string(s)
-	var result = make([]interface{}, len(strs))
+func (ss StringSlice) Equal(val1, val2 interface{}) bool { return val1.(string) == val2.(string) }
+func (ss StringSlice) Elements() []interface{} {
+	var strs = []string(ss)
+	var result = []interface{}{}
 	for _, s := range strs {
 		result = append(result, s)
 	}
 	return result
 }
-func (s StringSlice) Add(val interface{}) interface{} {
-	s = append([]string(s), val.(string))
-	return s
+func (ss StringSlice) Add(val interface{}) interface{} {
+	var result = append([]string(ss), val.(string))
+	return StringSlice(result)
 }
-func (s StringSlice) Len() int { return len([]string(s)) }
+func (ss StringSlice) Len() int { return len([]string(ss)) }
+func (ss StringSlice) Remove(val interface{}) interface{} {
+	var strs = []string(ss)
+	var result = []string{}
+	for _, s := range strs {
+		if ss.Equal(s, val) {
+			continue
+		}
+		result = append(result, s)
+	}
+	return result
+}
 
 func AddToStrings(set []string, val string) []string {
-	return []string((Add(StringSlice(set), val).(StringSlice)))
+	var result = Add(StringSlice(set), val).(StringSlice)
+	return []string(result)
 }
 
-func RemoveFromStrings(set []string, val string) {
-	Remove(StringSlice(set), val)
+func RemoveFromStrings(set []string, val string) []string {
+	return Remove(StringSlice(set), val).([]string)
 }
--- set/set_test.go
+++ set/set_test.go
@@ -7,13 +7,18 @@ import (
 
 func TestSet(t *testing.T) {
 	var strs = []string{}
-	strs =AddToStrings(strs, "a")
-	AddToStrings(strs, "b")
-	AddToStrings(strs, "c")
-	AddToStrings(strs, "d")
-	AddToStrings(strs, "e")
+	strs = AddToStrings(strs, "a")
+	strs = AddToStrings(strs, "b")
+	strs = AddToStrings(strs, "c")
+	strs = AddToStrings(strs, "d")
+	strs = AddToStrings(strs, "e")
 
+	// check
 	var checkStrs = []string{"a", "b", "c", "d", "e"}
+	if Len(StringSlice(strs)) != len(checkStrs) {
+		t.Error(len(StringSlice(strs)))
+		return
+	}
 	if len(strs) != len(checkStrs) {
 		t.Error(len(strs), strs)
 		return
@@ -24,5 +29,107 @@ func TestSet(t *testing.T) {
 			return
 		}
 	}
+	fmt.Println("---------- 1 ----------")
+
+	strs = AddToStrings(strs, "a")
+	strs = AddToStrings(strs, "b")
+	strs = AddToStrings(strs, "c")
+	strs = AddToStrings(strs, "d")
+	strs = AddToStrings(strs, "e")
+	strs = AddToStrings(strs, "f")
+
+	// check
+	checkStrs = []string{"a", "b", "c", "d", "e", "f"}
+	if Len(StringSlice(strs)) != len(checkStrs) {
+		t.Error(len(StringSlice(strs)))
+		return
+	}
+	if len(strs) != len(checkStrs) {
+		t.Error(len(strs), strs)
+		return
+	}
+	for i, s := range strs {
+		if s != checkStrs[i] {
+			t.Error(i, s)
+			return
+		}
+	}
+
+	fmt.Println("---------- 2 ----------")
+
+	AddToStrings(strs, "z")
+	AddToStrings(strs, "x")
+	AddToStrings(strs, "c")
+	AddToStrings(strs, "v")
+	AddToStrings(strs, "b")
+
+	// check
+	checkStrs = []string{"a", "b", "c", "d", "e", "f"}
+	if Len(StringSlice(strs)) != len(checkStrs) {
+		t.Error(len(StringSlice(strs)))
+		return
+	}
+	if len(strs) != len(checkStrs) {
+		t.Error(len(strs), strs)
+		return
+	}
+	for i, s := range strs {
+		if s != checkStrs[i] {
+			t.Error(i, s)
+			return
+		}
+	}
+
+	fmt.Println("---------- 3 ----------")
+
+	// remove element
+	strs = RemoveFromStrings(strs, "a")
+	strs = RemoveFromStrings(strs, "b")
+	strs = RemoveFromStrings(strs, "c")
+	strs = RemoveFromStrings(strs, "d")
+
+	// check
+	checkStrs = []string{"e", "f"}
+	if Len(StringSlice(strs)) != len(checkStrs) {
+		t.Error(len(StringSlice(strs)))
+		return
+	}
+	if len(strs) != len(checkStrs) {
+		t.Error(len(strs), strs)
+		return
+	}
+	for i, s := range strs {
+		if s != checkStrs[i] {
+			t.Error(i, s)
+			return
+		}
+	}
+
+	fmt.Println("---------- 4 ----------")
+	RemoveFromStrings(strs, "a")
+	RemoveFromStrings(strs, "b")
+	RemoveFromStrings(strs, "c")
+	RemoveFromStrings(strs, "d")
+	RemoveFromStrings(strs, "d")
+	RemoveFromStrings(strs, "e")
+	RemoveFromStrings(strs, "f")
+
+	// check
+	checkStrs = []string{"e", "f"}
+	if Len(StringSlice(strs)) != len(checkStrs) {
+		t.Error(len(StringSlice(strs)))
+		return
+	}
+	if len(strs) != len(checkStrs) {
+		t.Error(len(strs), strs)
+		return
+	}
+	for i, s := range strs {
+		if s != checkStrs[i] {
+			t.Error(i, s)
+			return
+		}
+	}
+
 	fmt.Println("TestSet test success!!!")
 }
